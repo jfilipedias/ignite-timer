3f9a0b8e7fe7e480433224051a7612d6095a7c6a
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-11-07 08:37:59 -0300

    Starts to implements the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Timer</h1>
+    </ThemeProvider>
+  )
 }
