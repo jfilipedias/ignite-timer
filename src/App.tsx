db072fac5cb81bedfd9aa6dfb0a0b55a185232e0
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-11-07 12:49:46 -0300

    Setups color and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Timer</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
