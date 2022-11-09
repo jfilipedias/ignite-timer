225c3f73660347801e90005586a7eabe22a652a8
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-11-08 21:50:05 -0300

    Starts to implements the history page

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 3.5rem;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    min-width: 600px;
+    border-collapse: collapse;
+
+    th {
+      padding: 1rem;
+      background-color: ${(props) => props.theme['gray-600']};
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      text-align: left;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+        border-top-right-radius: 8px;
+      }
+    }
+
+    td {
+      padding: 1rem;
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 2px solid ${(props) => props.theme['gray-800']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
